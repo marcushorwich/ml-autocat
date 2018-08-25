a3d21a71424a3da5a234f066c218d8721fabdda9
Calvin De Lima <calvindelima@bitbucket.org>
2018-08-25 16:16:38 -0400

    Moved core model functions into Model and added pipeline v1 function

--- autocat/models/pipelines.py
+++ autocat/models/pipelines.py
@@ -1,6 +1,16 @@
-def get_scaled_pipeline_v1():
+def get_scaled_pipeline_v1(X):
+  """ Returns the first iteration pipeline for this project
+    X: pandas.DataFrame
+        The feature matrix as a pandas DataFrame with named columns
+
+    Returns
+    -------
+    sklearn.Pipeline
+        Pipeline with the steps required to transform a dataset that looks like 
+        `X` to a feature matrix.
+  """
   from sklearn.pipeline import Pipeline
-  from ...data.features import CombinedFeatureAdder, feature_transactions_per_day, PandasDataFrameTransformer
+  from ..data.features import CombinedFeatureAdder, feature_transactions_per_day, PandasDataFrameTransformer
   from sklearn.preprocessing import StandardScaler
   
   # Create a pipeline to transform the modelling data
@@ -9,6 +19,5 @@ def get_scaled_pipeline_v1():
       ('std_scaler', StandardScaler()),
       ('data_frame', PandasDataFrameTransformer(list(X.columns) + ['TransactionsPerDay']))
   ])
-  print(pipeline.steps)
-
-get_scaled_pipeline_v1()
+  # print(pipeline.steps)
+  return pipeline
